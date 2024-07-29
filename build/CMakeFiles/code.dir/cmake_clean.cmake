d1460c523256d9403279d734e5972f5c17ca6c9d
LOST_TREE <147401619+HENG958@users.noreply.github.com>
2024-07-29 15:38:28 +0800

    test1

---
+++ build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/src/main.cpp.o"
+  "CMakeFiles/code.dir/src/main.cpp.o.d"
+  "CMakeFiles/code.dir/src/memory.cpp.o"
+  "CMakeFiles/code.dir/src/memory.cpp.o.d"
+  "CMakeFiles/code.dir/src/parser.cpp.o"
+  "CMakeFiles/code.dir/src/parser.cpp.o.d"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/code.dir/src/parser.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/code.dir/src/parser.cpp.o: /home/lzh/CPU/src/parser.cpp \
+ /usr/include/stdc-predef.h /home/lzh/CPU/src/parser.hpp \
+ /home/lzh/CPU/src/operation.hpp
