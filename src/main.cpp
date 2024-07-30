22c124dd3f038c7e6d6960668f516202a2fe5ea0
LOST_TREE <147401619+HENG958@users.noreply.github.com>
2024-07-30 14:50:38 +0800

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "tomasulo.hpp"
 Tomasulo CPU;
 int main() {
-    freopen("../testcases/array_test2.data", "r", stdin);
-    freopen("test1.out", "w", stdout);
+    //freopen("../testcases/array_test2.data", "r", stdin);
+    //freopen("test1.out", "w", stdout);
     CPU.Run();
     return 0;
 }
